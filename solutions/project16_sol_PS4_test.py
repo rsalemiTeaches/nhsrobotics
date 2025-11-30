7173111fa9df24d53e70fb95260076102715c805
Ray Salemi <ray@raysalemi.com>
2025-11-30 10:02:02 -0500

    Now with the first version of student code.

--- solutions/project16_sol_PS4_test.py
+++ solutions/project16_sol_PS4_test.py
@@ -1,10 +1,9 @@
 # Project 16: Remote Control Link
-# Architecture: Controller (Driver) -> Lambda Bridge -> Button Class (Logic)
+# Architecture: Controller -> Lambda -> Button
 
 from arduino_alvik import ArduinoAlvik
-from controller import Controller
-from button_class import Button 
-from nanoled import NanoLED 
+# NOW: We import all our tools from your single library!
+from nhs_robotics import NanoLED, Button, Controller
 import machine
 import ubinascii
 import time
@@ -20,8 +19,7 @@ alvik.left_led.set_color(1, 1, 0) # Yellow = Booting
 
 # 2. Setup NanoLED
 nano_led = NanoLED()
-# Use set_color(1, 1, 0) just like the Alvik LEDs!
-# We set it to Yellow so we can dim/brighten it later.
+# Set color to Yellow so we can control brightness later
 nano_led.set_color(1, 1, 0) 
 nano_led.off() 
 
@@ -36,6 +34,7 @@ ctl = Controller(ssid=MY_NAME)
 ctl.begin()
 
 # 4. Create Button Objects (THE LAMBDA BRIDGE)
+# We map specific controller buttons to our Button class
 cross_btn    = Button(lambda: ctl.buttons['cross'])
 triangle_btn = Button(lambda: ctl.buttons['triangle'])
 bumper_R1    = Button(lambda: ctl.buttons['R1'])
@@ -47,21 +46,23 @@ alvik.right_led.set_color(0, 0, 1)
 
 # --- MAIN LOOP ---
 while True:
+    # A. Update Data
     ctl.update()
     
-    # A. Driving
+    # B. Driving
     speed_L = ctl.left_stick_y * 50
     speed_R = ctl.right_stick_y * 50
     alvik.set_wheels_speed(speed_L, speed_R)
     
-    # B. Analog Brightness
+    # C. Analog Brightness
+    # ctl.R2 is 0.0 to 1.0. We convert to 0-100%
     if ctl.R2 < 0.05:
         nano_led.off()
     else:
         brightness_percent = int(ctl.R2 * 100)
         nano_led.set_brightness(brightness_percent)
     
-    # C. Events
+    # D. Events
     if cross_btn.get_touch():
         print("ACTION: Red Headlights")
         alvik.left_led.set_color(1, 0, 0)
@@ -71,23 +72,18 @@ while True:
         print("ACTION: Green LEDs")
         alvik.left_led.set_color(0, 1, 0) 
         alvik.right_led.set_color(0, 1, 0)
-        # NanoLED match (Green)
         nano_led.set_color(0, 1, 0)
         
     if bumper_R1.get_touch():
         print("ACTION: Blue LEDs")
         alvik.left_led.set_color(0, 0, 1)
         alvik.right_led.set_color(0, 0, 1)
-        # NanoLED match (Blue)
         nano_led.set_color(0, 0, 1)
 
     if up_btn.get_touch():
         print("ACTION: Purple LEDs")
-        # Purple = Red + Blue
         alvik.left_led.set_color(1, 0, 1)
         alvik.right_led.set_color(1, 0, 1)
-        # NanoLED match (Purple)
         nano_led.set_color(1, 0, 1)
         
     time.sleep(0.01)
-    
