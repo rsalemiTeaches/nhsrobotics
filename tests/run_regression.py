2558a8d5d7278991890000b22c1a72d98d7ba915
Ray Salemi <rsalemi@natickps.org>
2026-05-15 08:21:14 -0400

    fix test

--- tests/run_regression.py
+++ tests/run_regression.py
@@ -2,7 +2,11 @@
 
 from arduino_alvik import ArduinoAlvik
 from nhs_robotics import SuperBot
+import sys
 
+# Append the root-level tests directory to the sys.path list
+if "/tests" not in sys.path:
+    sys.path.append("/tests")
 from regression_utils import RegressionRunner
 import regression_logic
 import regression_hardware
